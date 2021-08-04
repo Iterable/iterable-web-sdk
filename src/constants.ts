4b514a44f7efbbd04fd6689af308771fc7f4ec2e
mmckenna <mmckenna.phila@gmail.com>
2021-08-04 13:01:55 -0400

    adds circular dependency check for build

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
+import { getInAppMessages } from 'src/inapp';
+
+export const thing = getInAppMessages({ count: 20 });
+
 /* number of MS to wait between in-app messages to show the next one */
 export const DISPLAY_INTERVAL_DEFAULT = 30000;
