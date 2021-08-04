370c1a9fc6ee3433ea17b30d8a9d54c8bd9f7e4b
mmckenna <mmckenna.phila@gmail.com>
2021-08-04 13:29:05 -0400

    removes test code

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,2 @@
-/* eslint-disable-next-line */
-// @ts-ignore
-import { getInAppMessages } from './inapp';
-
-export const thing = getInAppMessages;
-
 /* number of MS to wait between in-app messages to show the next one */
 export const DISPLAY_INTERVAL_DEFAULT = 30000;
