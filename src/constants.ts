103b39d89990c8f17f0fb962974c1b6d244a53ae
mmckenna <mmckenna.phila@gmail.com>
2021-08-04 13:17:38 -0400

    update logic

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
-import { getInAppMessages } from 'src/inapp';
+/* eslint-disable-next-line */
+// @ts-ignore
+import { getInAppMessages } from './inapp';
 
-export const thing = getInAppMessages({ count: 20 });
+export const thing = getInAppMessages;
 
 /* number of MS to wait between in-app messages to show the next one */
 export const DISPLAY_INTERVAL_DEFAULT = 30000;
