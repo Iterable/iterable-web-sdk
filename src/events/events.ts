6f5eee6cd4ce02586a41928891d8239d9069bdab
mmckenna <mmckenna.phila@gmail.com>
2021-09-23 13:33:56 -0700

    fixes user agent

--- src/events/events.ts
+++ src/events/events.ts
@@ -51,7 +51,8 @@ export const trackInAppOpen = (
       ...payload,
       deviceInfo: {
         ...payload.deviceInfo,
-        platform: WEB_PLATFORM
+        platform: WEB_PLATFORM,
+        deviceId: global.navigator.userAgent || ''
       }
     }
   });
