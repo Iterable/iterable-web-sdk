9b0736c89453dc26d0a7b4be8a5e5fe38a24c618
Marty <martin.mckenna@iterable.com>
2021-09-27 10:32:56 -0700

    handles updateEmail invocations (#43)

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,12 +1,16 @@
 import './styles/index.css';
 import axios from 'axios';
-import { initIdentify, getInAppMessages } from '@iterable/web-sdk';
+import {
+  initIdentify,
+  getInAppMessages,
+  updateUserEmail
+} from '@iterable/web-sdk';
 
 ((): void => {
   /* set token in the SDK */
   const { setEmail, logout } = initIdentify(
     process.env.API_KEY || '',
-    (email) => {
+    ({ email }) => {
       return axios
         .post(
           'http://localhost:5000/generate',
@@ -38,6 +42,97 @@ import { initIdentify, getInAppMessages } from '@iterable/web-sdk';
   );
 
   const startBtn = document.getElementById('start');
+  const loginBtn = document.getElementById('login');
+  const changeEmailForm = document.getElementById('change-email-form');
+  const changeEmailBtn = document.getElementById('change-email-btn');
+
+  const handleGetMessagesClick = (event: MouseEvent) => {
+    event.preventDefault();
+    if (startBtn.getAttribute('aria-disabled') !== 'true') {
+      startBtn.innerText = `Loading...`;
+      startBtn.setAttribute('aria-disabled', 'true');
+      startBtn.className = 'disabled';
+      request()
+        .then((response) => {
+          startBtn.innerText = `${response.data.inAppMessages.length} total messages retrieved!`;
+        })
+        .catch(console.warn);
+    }
+  };
+
+  const handleLoginClick = (event: MouseEvent) => {
+    const email =
+      localStorage.getItem('iterable-email') || 'iterable.tester@gmail.com';
+
+    /* disable login btn */
+    if (loginBtn.getAttribute('aria-disabled') !== 'true') {
+      event.preventDefault();
+      /* login */
+      loginBtn.setAttribute('aria-disabled', 'true');
+      loginBtn.className = 'disabled';
+      loginBtn.innerText = `Loading...`;
+      setEmail(email).then(() => {
+        /* enable change email button */
+        changeEmailBtn.classList.remove('disabled');
+        changeEmailBtn.setAttribute('aria-disabled', 'false');
+
+        changeEmailBtn.innerText = 'Change email';
+
+        /* enable in-app message button */
+        loginBtn.innerText = `Logged in as ${email}`;
+        startBtn.setAttribute('aria-disabled', 'false');
+        startBtn.classList.remove('disabled');
+        startBtn.innerText = 'Start painting in-app messages';
+        /* aria-disabled doesn't actually disable the button lol */
+
+        startBtn.addEventListener('click', handleGetMessagesClick);
+      });
+    }
+  };
+
+  const handleChangeEmail = (event: MouseEvent) => {
+    event.preventDefault();
+    const inputField = document.getElementById(
+      'change-email-input'
+    ) as HTMLInputElement;
+    const newEmail = inputField.value;
+
+    changeEmailBtn.setAttribute('aria-disabled', 'true');
+    changeEmailBtn.className = 'disabled';
+    changeEmailBtn.innerText = `Loading...`;
+
+    startBtn.setAttribute('aria-disabled', 'true');
+    startBtn.className = 'disabled';
+    startBtn.removeEventListener('click', handleGetMessagesClick);
+
+    updateUserEmail(newEmail)
+      .then(() => {
+        localStorage.setItem('iterable-email', newEmail);
+        loginBtn.innerText = `Logged in as ${newEmail}`;
+
+        changeEmailBtn.setAttribute('aria-disabled', 'false');
+        changeEmailBtn.classList.remove('disabled');
+        changeEmailBtn.innerText = 'Change email';
+        inputField.value = '';
+
+        startBtn.setAttribute('aria-disabled', 'false');
+        startBtn.classList.remove('disabled');
+
+        startBtn.addEventListener('click', handleGetMessagesClick);
+      })
+      .catch(() => {
+        inputField.value = 'Something went wrong.';
+        changeEmailBtn.setAttribute('aria-disabled', 'false');
+        changeEmailBtn.classList.remove('disabled');
+        changeEmailBtn.innerText = 'Change email';
+
+        startBtn.setAttribute('aria-disabled', 'false');
+        startBtn.classList.remove('disabled');
+      });
+  };
+
+  loginBtn.addEventListener('click', handleLoginClick);
+  changeEmailForm.addEventListener('submit', handleChangeEmail);
 
   document
     .getElementById('pause')
@@ -50,25 +145,17 @@ import { initIdentify, getInAppMessages } from '@iterable/web-sdk';
     .addEventListener('click', pauseMessageStream);
   document.getElementById('logout').addEventListener('click', () => {
     logout();
-    startBtn.innerText = 'Start Auto-Painting In-App Messages';
-    startBtn.setAttribute('aria-disabled', 'false');
-    startBtn.className = '';
-  });
-
-  startBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (startBtn.getAttribute('aria-disabled') !== 'true') {
-      startBtn.innerText = `Loading...`;
-      setEmail('iterable.tester@gmail.com').then(() => {
-        /* aria-disabled doesn't actually disable the button lol */
-        request()
-          .then((response) => {
-            startBtn.innerText = `${response.data.inAppMessages.length} total messages retrieved!`;
-          })
-          .catch(console.warn);
-      });
-    }
+    startBtn.innerText = 'Login to See In-App Messages';
     startBtn.setAttribute('aria-disabled', 'true');
     startBtn.className = 'disabled';
+
+    loginBtn.classList.remove('disabled');
+    loginBtn.setAttribute('aria-disabled', 'false');
+    loginBtn.innerText = 'Login';
+
+    changeEmailBtn.setAttribute('aria-disabled', 'true');
+    changeEmailBtn.className = 'disabled';
+
+    startBtn.removeEventListener('click', handleGetMessagesClick);
   });
 })();
--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,5 @@ export const DISPLAY_INTERVAL_DEFAULT = 30000;
 /* how many times we try to create a new user when _setUserID_ is invoked */
 export const RETRY_USER_ATTEMPTS = 0;
 
+/* code that comes back on 401 responses from Iterable API */
 export const INVALID_JWT_CODE = 'InvalidJwtPayload';
