2426dab5c7a178473d3aed2498b690a8406aa4af
mmckenna <mmckenna.phila@gmail.com>
2021-10-20 11:35:12 -0700

    updates constants

--- src/constants.ts
+++ src/constants.ts
@@ -10,6 +10,7 @@ export const BASE_URL = process.env.BASE_URL || 'https://api.iterable.com/api';
   API payload _platform_ param which is send up automatically 
   with tracking and getMessage requests 
 */
-export const WEB_PLATFORM = 'iOS';
+export const WEB_PLATFORM = 'Web';
 
-export const ANIMATION_DURATION = 100;
+/* how long animations fade/side in for. */
+export const ANIMATION_DURATION = 400;
