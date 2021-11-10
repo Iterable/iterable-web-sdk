a32492a16923d0e10991b72b0865f20073997334
mmckenna <mmckenna.phila@gmail.com>
2021-11-10 14:20:27 -0800

    delete build files

--- dist/authorization/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const getEpochExpiryTimeInMS: (jwt: string) => any;
-export declare const getEpochDifferenceInMS: (epochNow: number, epochFuture: number) => number;
--- dist/commerce/commerce.d.ts
+++
@@ -1,4 +0,0 @@
-import { TrackPurchaseRequestParams, UpdateCartRequestParams } from './types';
-import { IterableResponse } from '../types';
-export declare const updateCart: (payload: UpdateCartRequestParams) => import("../types").IterablePromise<IterableResponse>;
-export declare const trackPurchase: (payload: TrackPurchaseRequestParams) => import("../types").IterablePromise<IterableResponse>;
