d50b2af553c6b5f788ed667f57fc8fd3ee05165e
Paul Jung <paul.jung@iterable.com>
2022-10-13 12:57:53 -0400

    [MOB-4955] Fix target attribute conditional + refactor handleLinks (#169)
    * [MOB-4955] Fix target attribute conditional
    
    * [MOB-4955] Refactor handleLinks
    
    * [MOB-4955] Move comment
    
    * [MOB-4955] Handle extra condition outside of shared function
    
    * [MOB-4955] Fix default case for non-safari link handling
    
    * [MOB-4955] Update comment

--- src/inapp/inapp.ts
+++ src/inapp/inapp.ts
@@ -481,6 +481,35 @@ export function getInAppMessages(
               clickedHostname === global.location.host || !clickedHostname;
             const { handleLinks } = payload;
 
+            /* 
+              if the _handleLinks_ option is set, we need to open links 
+              according to that enum. So the way this works is:
+
+              1. If _open-all-same-tab, then open every link in the same tab
+              2. If _open-all-new-tab, open all in new tab
+              3. If _external-new-tab_, open internal links in same tab, otherwise new tab.
+
+              This was a fix to account for the fact that Bee editor templates force
+              target="_blank" on all links, so we gave this option as an escape hatch for that.
+            */
+            const manageHandleLinks = (
+              sameTabAction: () => void,
+              newTabAction: () => void
+            ) => {
+              if (typeof handleLinks === 'string') {
+                if (
+                  handleLinks === 'open-all-same-tab' ||
+                  (isInternalLink && handleLinks === 'external-new-tab')
+                ) {
+                  sameTabAction();
+                } else {
+                  newTabAction();
+                }
+              } else if (link.getAttribute('target') === null) {
+                sameTabAction();
+              }
+            };
+
             if (isDismissNode || isActionLink) {
               /* 
                 give the close anchor tag properties that make it 
@@ -493,24 +522,19 @@ export function getInAppMessages(
               Safari blocks all bound event handlers (including our click event handlers)
               in iframes, so links will not work in Safari unless we circumvent the
               restriction by appending target to each link tag.
+
+              NOTE: Because click event handlers cannot be attached to iframe links in
+              Safari, we cannot track in-app clicks in metrics.
             */
             if (isSafari) {
-              if (
-                !isIterableKeywordLink &&
-                link.getAttribute('target') === 'undefined'
-              ) {
-                if (typeof handleLinks === 'string') {
-                  if (
-                    handleLinks === 'open-all-same-tab' ||
-                    (isInternalLink && handleLinks === 'external-new-tab')
-                  ) {
-                    link.setAttribute('target', '_top');
-                  } else {
+              if (!isIterableKeywordLink) {
+                manageHandleLinks(
+                  () => link.setAttribute('target', '_top'),
+                  () => {
                     link.setAttribute('target', '_blank');
+                    link.setAttribute('rel', 'noopener noreferrer');
                   }
-                } else {
-                  link.setAttribute('target', '_top');
-                }
+                );
               }
             } else {
               link.addEventListener('click', (event) => {
@@ -579,42 +603,25 @@ export function getInAppMessages(
                     of the reserved iterable keyword links
                   */
                   if (!isIterableKeywordLink) {
-                    if (typeof handleLinks === 'string') {
-                      /* 
-                        if the _handleLinks_ option is set, we need to open links 
-                        according to that enum. So the way this works is:
-  
-                        1. If _open-all-same-tab, then open every link in the same tab
-                        2. If _open-all-new-tab, open all in new tab
-                        3. If _external-new-tab_, open internal links in same tab, otherwise new tab.
-  
-                        This was a fix to account for the fact that Bee editor templates force
-                        target="_blank" on all links, so we gave this option as an escape hatch for that.
-                      */
-                      if (
-                        handleLinks === 'open-all-same-tab' ||
-                        (isInternalLink && handleLinks === 'external-new-tab')
-                      ) {
-                        global.location.assign(clickedUrl);
-                      } else {
+                    manageHandleLinks(
+                      () => global.location.assign(clickedUrl),
+                      () => {
                         global.open(
                           clickedUrl,
                           '_blank',
                           'noopener,noreferrer'
                         );
                       }
-                    } else if (openInNewTab) {
-                      /**
-                        Using target="_blank" without rel="noreferrer" and rel="noopener"
-                        makes the website vulnerable to window.opener API exploitation attacks
-  
-                        @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a
-                      */
+                    );
+                    /**
+                      Using target="_blank" without rel="noreferrer" and rel="noopener"
+                      makes the website vulnerable to window.opener API exploitation attacks
+                      
+                      @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#security_and_privacy
+                    */
+                    if (openInNewTab)
                       global.open(clickedUrl, '_blank', 'noopener,noreferrer');
-                    } else {
-                      /* otherwise just link them in the same tab */
-                      global.location.assign(clickedUrl);
-                    }
+                    else global.location.assign(clickedUrl);
                   }
                 }
               });
