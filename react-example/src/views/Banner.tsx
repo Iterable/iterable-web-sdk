00c7c12d25bfec47235f88e9b0291dde32ba48cb
iamavishkar <imavishkar@gmail.com>
2023-08-21 13:20:09 +0530

    usage of banner view

---
+++ react-example/src/views/Banner.tsx
@@ -0,0 +1,14 @@
+import Banner from '../../../src/components/banner';
+
+export const BannerView = () => {
+  return (
+    <Banner
+      body="Hello World!"
+      buttonText="OK"
+      heading="A banner view interface"
+      logo="https://www.google.com/images/branding/googlelogo/1x/googlelogo_dark_color_272x92dp.png"
+    />
+  );
+};
+
+export default Banners;
