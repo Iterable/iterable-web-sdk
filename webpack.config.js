c0f47e970a207ed57a8ba6ca67bdd90cee2bc754
Vishal Joshi <joshivishal895@gmail.com>
2023-09-18 15:01:42 +0530

    Embedded msg configurations changed

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const env = require('dotenv').config({ path: './.env' });
 const webpack = require('webpack');
 const { version } = require('./package.json');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 function getParsedEnv() {
   if (!env.error) {
@@ -16,7 +17,7 @@ function getParsedEnv() {
 
 module.exports = {
   mode: 'production',
-  entry: './dist/index.js',
+  entry: './src/index.ts',
   output: {
     filename: './index.js',
     path: path.resolve(__dirname),
@@ -26,6 +27,25 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(getParsedEnv())
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'dist/components/style.css' // Output CSS filename
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css']
+  }
 };
