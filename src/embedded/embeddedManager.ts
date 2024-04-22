8c51ad20ebdbcb842900b1281878d234a2ad29bb
hardikmashru <hardikmashru@iterable.com>
2024-04-22 11:10:48 +0530

    HTTP call in retrieveEmbeddedMessages

--- src/embedded/embeddedManager.ts
+++ src/embedded/embeddedManager.ts
@@ -9,13 +9,14 @@ import { IEmbeddedMessage } from '../events/embedded/types';
 import { EmbeddedMessagingProcessor } from './embeddedMessageProcessor';
 import { embedded_msg_endpoint, ErrorMessage } from './consts';
 import { trackEmbeddedMessageReceived } from 'src/events/embedded/events';
-import { functions } from 'src/utils/functions';
 import { IterableActionRunner } from 'src/utils/IterableActionRunner';
 import {
   URL_SCHEME_ITBL,
   URL_SCHEME_ACTION,
   URL_SCHEME_OPEN,
-  WEB_PLATFORM
+  WEB_PLATFORM,
+  SHARED_PREF_USER_ID,
+  SHARED_PREF_EMAIL
 } from '../constants';
 import { IterableEmbeddedMessage } from './embeddedMessage';
 import { EndPoints } from 'src/events/consts';
@@ -25,8 +26,17 @@ export class EmbeddedManager {
   private messages: IEmbeddedMessage[] = [];
   private updateListeners: EmbeddedMessageUpdateHandler[] = [];
 
+  private addEmailOrUserIdToJson(jsonParams: any): any {
+    const userId = localStorage.getItem(SHARED_PREF_USER_ID);
+    const email = localStorage.getItem(SHARED_PREF_EMAIL);
+    if (userId) {
+      jsonParams.userId = userId;
+    } else if (email) {
+      jsonParams.email = email;
+    }
+    return jsonParams;
+  }
   public async syncMessages(
-    userIdOrEmail: string,
     platform: string,
     sdkVersion: string,
     packageName: string,
@@ -34,7 +44,6 @@ export class EmbeddedManager {
     placementIds?: number[]
   ) {
     await this.retrieveEmbeddedMessages(
-      userIdOrEmail,
       platform,
       sdkVersion,
       packageName,
@@ -44,7 +53,6 @@ export class EmbeddedManager {
   }
 
   private async retrieveEmbeddedMessages(
-    userIdOrEmail: string,
     platform: string,
     sdkVersion: string,
     packageName: string,
@@ -52,17 +60,23 @@ export class EmbeddedManager {
   ) {
     try {
       let url = `${embedded_msg_endpoint}?`;
-      url += `platform=${platform}`;
-      url += `&sdkVersion=${sdkVersion}`;
-      url += `&packageName=${packageName}`;
-
+      let params: any = {};
+      params = this.addEmailOrUserIdToJson(params);
       if (placementIds.length > 0) {
-        url += placementIds.map((id) => `&placementIds=${id}`).join('');
+        params.placementIds = placementIds
+          .map((id) => `&placementIds=${id}`)
+          .join('');
       }
       url = url.replace(/&$/, '');
       const iterableResult: any = await baseIterableRequest<IterableResponse>({
         method: 'GET',
-        url: url
+        url: url,
+        params: {
+          ...params,
+          platform: platform,
+          sdkVersion: sdkVersion,
+          packageName: packageName
+        }
       });
       const embeddedMessages = this.getEmbeddedMessages(
         iterableResult?.data?.placements || []
@@ -73,7 +87,7 @@ export class EmbeddedManager {
           this.getEmbeddedMessages(iterableResult?.data?.placements)
         );
         this.setMessages(processor);
-        await this.trackNewlyRetrieved(processor, userIdOrEmail);
+        await this.trackNewlyRetrieved(processor);
         this.messages = [
           ...this.getEmbeddedMessages(iterableResult?.data?.placements)
         ];
@@ -113,21 +127,15 @@ export class EmbeddedManager {
     });
   }
 
-  private async trackNewlyRetrieved(
-    _processor: EmbeddedMessagingProcessor,
-    userIdOrEmail: string
-  ) {
+  private async trackNewlyRetrieved(_processor: EmbeddedMessagingProcessor) {
     const msgsList = _processor.newlyRetrievedMessages();
     if (msgsList.length > 0) {
       this.notifyUpdateDelegates();
     }
     for (let i = 0; i < msgsList.length; i++) {
-      const messages = {} as IEmbeddedMessage;
+      let messages = {} as IEmbeddedMessage;
       messages.messageId = msgsList[i].metadata.messageId;
-
-      functions.checkEmailValidation(userIdOrEmail)
-        ? (messages.email = userIdOrEmail)
-        : (messages.userId = userIdOrEmail);
+      messages = this.addEmailOrUserIdToJson(messages);
       await trackEmbeddedMessageReceived(messages);
     }
   }
