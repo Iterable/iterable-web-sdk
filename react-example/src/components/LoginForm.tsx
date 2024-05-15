cccda093ca4ff998ef04d5d69e03e15485e0fb68
hardikmashru <hardik.mashru@iterable.com>
2024-05-15 18:51:12 +0530

    revert LoginForm changes

--- react-example/src/components/LoginForm.tsx
+++ react-example/src/components/LoginForm.tsx
@@ -81,7 +81,7 @@ export const LoginForm: FC<Props> = ({ setEmail, logout, refreshJwt }) => {
               onChange={(e) => updateEmail(e.target.value)}
               value={email}
               placeholder="e.g. hello@gmail.com"
-              //type="email"
+              type="email"
               required
             />
             <Button type="submit">Change</Button>
@@ -104,7 +104,7 @@ export const LoginForm: FC<Props> = ({ setEmail, logout, refreshJwt }) => {
             onChange={(e) => updateEmail(e.target.value)}
             value={email}
             placeholder="e.g. hello@gmail.com"
-            //type="email"
+            type="email"
             required
             data-qa-login-input
           />
