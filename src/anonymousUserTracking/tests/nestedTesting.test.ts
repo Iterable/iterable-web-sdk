4013132af7ffee18418802ef9ece6bede692529c
darshan-iterable <darshan.parmar@iterable.com>
2024-09-05 23:57:07 +0530

    updated nested field logic (#445)
    * updated nested field logic
    
    * Resolved typo
    
    * resolved conflicts

--- src/anonymousUserTracking/tests/nestedTesting.test.ts
+++ src/anonymousUserTracking/tests/nestedTesting.test.ts
@@ -1,5 +1,6 @@
 import { SHARED_PREFS_EVENT_LIST_KEY } from '../../constants';
 import CriteriaCompletionChecker from '../criteriaCompletionChecker';
+import { NESTED_CRITERIA } from './constants';
 
 const localStorageMock = {
   getItem: jest.fn(),
@@ -13,97 +14,6 @@ describe('nestedTesting', () => {
   });
 
   it('should return criteriaId 168 (nested field)', () => {
-    (localStorage.getItem as jest.Mock).mockImplementation((key) => {
-      if (key === SHARED_PREFS_EVENT_LIST_KEY) {
-        return JSON.stringify([
-          {
-            dataFields: {
-              email: 'user@example.com',
-              furniture: [
-                {
-                  furnitureType: 'Sofa',
-                  furnitureColor: 'White',
-                  lengthInches: 40,
-                  widthInches: 60
-                },
-                {
-                  furnitureType: 'Sofa',
-                  furnitureColor: 'Gray',
-                  lengthInches: 20,
-                  widthInches: 30
-                }
-              ]
-            },
-            eventType: 'user'
-          }
-        ]);
-      }
-      return null;
-    });
-
-    const localStoredEventList = localStorage.getItem(
-      SHARED_PREFS_EVENT_LIST_KEY
-    );
-
-    const checker = new CriteriaCompletionChecker(
-      localStoredEventList === null ? '' : localStoredEventList
-    );
-    const result = checker.getMatchedCriteria(
-      JSON.stringify({
-        count: 1,
-        criterias: [
-          {
-            criteriaId: '168',
-            name: 'nested testing',
-            createdAt: 1721251169153,
-            updatedAt: 1723488175352,
-            searchQuery: {
-              combinator: 'And',
-              searchQueries: [
-                {
-                  combinator: 'And',
-                  searchQueries: [
-                    {
-                      dataType: 'user',
-                      searchCombo: {
-                        combinator: 'And',
-                        searchQueries: [
-                          {
-                            dataType: 'user',
-                            field: 'furniture',
-                            comparatorType: 'IsSet',
-                            value: '',
-                            fieldType: 'nested'
-                          },
-                          {
-                            dataType: 'user',
-                            field: 'furniture.furnitureColor',
-                            comparatorType: 'IsSet',
-                            value: '',
-                            fieldType: 'string'
-                          },
-                          {
-                            dataType: 'user',
-                            field: 'furniture.furnitureType',
-                            comparatorType: 'Equals',
-                            value: 'Sofa',
-                            fieldType: 'string'
-                          }
-                        ]
-                      }
-                    }
-                  ]
-                }
-              ]
-            }
-          }
-        ]
-      })
-    );
-    expect(result).toEqual('168');
-  });
-
-  it('should return criteriaId 168 (nested field - No match)', () => {
     (localStorage.getItem as jest.Mock).mockImplementation((key) => {
       if (key === SHARED_PREFS_EVENT_LIST_KEY) {
         return JSON.stringify([
@@ -139,58 +49,47 @@ describe('nestedTesting', () => {
     const checker = new CriteriaCompletionChecker(
       localStoredEventList === null ? '' : localStoredEventList
     );
-    const result = checker.getMatchedCriteria(
-      JSON.stringify({
-        count: 1,
-        criterias: [
+    const result = checker.getMatchedCriteria(JSON.stringify(NESTED_CRITERIA));
+    expect(result).toEqual('168');
+  });
+
+  it('should return criteriaId 168 (nested field - No match)', () => {
+    (localStorage.getItem as jest.Mock).mockImplementation((key) => {
+      if (key === SHARED_PREFS_EVENT_LIST_KEY) {
+        return JSON.stringify([
           {
-            criteriaId: '168',
-            name: 'nested testing',
-            createdAt: 1721251169153,
-            updatedAt: 1723488175352,
-            searchQuery: {
-              combinator: 'And',
-              searchQueries: [
+            dataFields: {
+              email: 'user@example.com',
+              furniture: [
                 {
-                  combinator: 'And',
-                  searchQueries: [
-                    {
-                      dataType: 'user',
-                      searchCombo: {
-                        combinator: 'And',
-                        searchQueries: [
-                          {
-                            dataType: 'user',
-                            field: 'furniture',
-                            comparatorType: 'IsSet',
-                            value: '',
-                            fieldType: 'nested'
-                          },
-                          {
-                            dataType: 'user',
-                            field: 'furniture.furnitureColor',
-                            comparatorType: 'IsSet',
-                            value: '',
-                            fieldType: 'string'
-                          },
-                          {
-                            dataType: 'user',
-                            field: 'furniture.furnitureType',
-                            comparatorType: 'Equals',
-                            value: 'Sofa',
-                            fieldType: 'string'
-                          }
-                        ]
-                      }
-                    }
-                  ]
+                  furnitureType: 'Sofa',
+                  furnitureColor: 'Gray',
+                  lengthInches: 40,
+                  widthInches: 60
+                },
+                {
+                  furnitureType: 'table',
+                  furnitureColor: 'White',
+                  lengthInches: 20,
+                  widthInches: 30
                 }
               ]
-            }
+            },
+            eventType: 'user'
           }
-        ]
-      })
+        ]);
+      }
+      return null;
+    });
+
+    const localStoredEventList = localStorage.getItem(
+      SHARED_PREFS_EVENT_LIST_KEY
     );
+
+    const checker = new CriteriaCompletionChecker(
+      localStoredEventList === null ? '' : localStoredEventList
+    );
+    const result = checker.getMatchedCriteria(JSON.stringify(NESTED_CRITERIA));
     expect(result).toEqual(null);
   });
 });
